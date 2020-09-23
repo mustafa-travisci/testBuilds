11d17a170a6c8f49dbb4d76938a7b9a720905955
Mustafa Ergul <mustafa@travis-ci.org>
2020-09-23 15:39:18 +0300

    heroku key test

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-public_dns = ec2.99999999999.compute.amazonaws.com
-echo 'Hello'
-echo 'World'
+a=1
+b=3
+if [[ "$a" -lt 0 || "$b" -gt 2 ]]
+then
+    echo "all_went_true"
+elif [[ $a==2 || $b==4 ]]
+then
+    echo "first_retry"
+    echo "second_retry"
+else
+    echo "NON"
+fi
+
